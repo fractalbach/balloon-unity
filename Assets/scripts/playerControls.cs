471ad87892890f623fb27c1cec5bd79371fc4752
fractalbach <fractalbach@gmail.com>
2019-04-06 04:48:05 -0700

    init

---
+++ Assets/scripts/playerControls.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerControls : MonoBehaviour
+{
+	public float speed = 5f;
+	public float maxSpeed = 5f;
+	private Rigidbody2D rb;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody2D> ();
+	}
+	
+	void FixedUpdate()
+	{	
+		if (rb.velocity.magnitude > maxSpeed) {
+			rb.velocity = rb.velocity.normalized * maxSpeed;
+			return;
+		}
+		float moveX = Input.GetAxis ("Horizontal");
+		float moveY = Input.GetAxis ("Vertical");
+		Vector2 movement = new Vector2 (moveX, moveY);
+		rb.AddForce (movement * speed);
+	}
+
+}
