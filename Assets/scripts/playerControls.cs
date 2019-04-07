6533c896bdd9f3fb561d44ac1733135638f5558b
fractalbach <fractalbach@gmail.com>
2019-04-06 21:22:30 -0700

    added new sprites and a slash animation

--- Assets/scripts/playerControls.cs
+++ Assets/scripts/playerControls.cs
@@ -7,14 +7,26 @@ public class playerControls : MonoBehaviour
 	public float speed = 5f;
 	public float maxSpeed = 5f;
 	private Rigidbody2D rb;
+	private Animator anim;
 
 	void Start()
 	{
 		rb = GetComponent<Rigidbody2D> ();
+		anim = GetComponent<Animator> ();
+	}
+	
+	void Update()
+	{
+		HandleSword();
 	}
 	
 	void FixedUpdate()
 	{	
+		HandleMovement();
+	}
+	
+	void HandleMovement()
+	{
 		if (rb.velocity.magnitude > maxSpeed) {
 			rb.velocity = rb.velocity.normalized * maxSpeed;
 			return;
@@ -25,4 +37,17 @@ public class playerControls : MonoBehaviour
 		rb.AddForce (movement * speed);
 	}
 
+
+	// Another potential input button/thing to use is:
+	// 	Input.GetButton("Fire1")
+	// but currently it has some unexpected results.	
+	void HandleSword()
+	{
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			anim.SetTrigger("Slash");
+		}
+	}
+
+	
+	
 }
